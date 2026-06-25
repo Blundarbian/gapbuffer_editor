f8d4d1e817817fcaa14fce3be9dacbd3b772b325
Blundarbian <Jambo3657@gmail.com>
2026-06-24 22:40:06 -0700

    gab_buffer init

--- display.c
+++ display.c
@@ -7,14 +7,21 @@
 // enum to hold init pairs
 enum { PAIR_ERR = 1, PAIR_BW, PAIR_WB, PAIR_BR };
 
+WINDOW *create_main_window(int cords[]);
+WINDOW *create_bar_window(int cords[]);
+WINDOW *create_menu_window(int cords[]);
+
 int main(int argc, char *argv[])
 {	
 	gap_buffer *gb;
 	WINDOW *main, *bar, *menu;
-	initscr();
 
-	int max_y, max_x;
-	getmaxyx(stdscr, max_y, max_x);
+	initscr();
+	noecho();
+	keypad(stdscr, TRUE);
+
+	int maxx, maxy;
+	getmaxyx(stdscr, maxy, maxx);
 
 	if (!has_colors()) 
 	{
@@ -27,6 +34,19 @@ int main(int argc, char *argv[])
 	init_pair(PAIR_BW, COLOR_WHITE, COLOR_BLACK);
 	init_pair(PAIR_WB, COLOR_BLACK, COLOR_WHITE);
 	init_pair(PAIR_BR, COLOR_BLACK, COLOR_RED);
+
+	if (argc == 2)	
+		gb = copyfiletobuffer(argv[1]);
+	else
+		gb = initgapbuffer(0);
+	
+	if (!gb)
+	{
+		fprintf(stderr, "error: gap_buffer cannot be created");
+		endwin();
+		return 0;
+	}
+
 
 	addstr("This is a testing\n");
 	refresh();
